ef666e84715207c648f7aa0e17f1cf4540ec7ce7
Ng Weng Shan <ngwengshan025@hotmail.com>
2024-01-29 22:47:36 +0800

    [add]:draw block when select leaf node

--- myCanvas.py
+++ myCanvas.py
@@ -25,12 +25,15 @@ class rtCanvas(MyCanvas):  # Canvas for Reference Tree
     x = 30
     y = 30
     view_support = False
+    subtree_color_list = [BLUE,RED,ORANGE,BEIGE,PURPLE]
+    subtree_color_used = [1,1,1,1,1]
     
     def __init__(self,tree,width, height,view_support):
         super().__init__(width = width, height = height)
         self.tree = tree
         self.view_support = view_support
         self.draw_rt(node = self.tree.seed_node)
+        self.on_mouse_down(self.mouse_clicked)
 
     def draw_rt(self,node=None,level=0):
         if node is None:
@@ -39,7 +42,7 @@ class rtCanvas(MyCanvas):  # Canvas for Reference Tree
         for child in node.child_node_iter():
             self.draw_rt(child, level + 1)
 
-        self.fill_style = 'black'
+        self.fill_style = BLACK
         self.font = LEAF_FONT
         if node.is_leaf():
             if self.view_support:
@@ -48,12 +51,13 @@ class rtCanvas(MyCanvas):  # Canvas for Reference Tree
                node.x = X_INTERVAL * level + X_INTERVAL
 
             node.y = self.y
-            self.begin_path()
-            self.move_to(node.x, node.y - 5)
-            self.line_to(node.x + X_INTERVAL, node.y - 5)
-            self.stroke()
-            self.fill_text(node.taxon.label, node.x + X_INTERVAL,node.y)
+            self.draw_leaf_node(node)
             self.y += Y_INTERVAL
+
+            node.x1 = node.x + X_INTERVAL - 2
+            node.x2 = node.x1 + X_INTERVAL + len(str(node.taxon.label)) * 7.5
+            node.y1 = node.y - NODE_BLOCK_HEIGHT - 2
+            node.y2 = node.y1 + NODE_BLOCK_HEIGHT + 6
 
             # testing
             # self.draw_dots(node.x + X_INTERVAL - 5 , node.y - NODE_BLOCK_HEIGH  T)
@@ -89,33 +93,82 @@ class rtCanvas(MyCanvas):  # Canvas for Reference Tree
                 self.fill_style = 'blue'
                 if node.label:
                     self.fill_text(node.label, node.x + 3, node.y - 8)
-                self.fill_style = 'black'
+                self.fill_style = BLACK
+
+            node.x1 = node.x + X_INTERVAL - 4
+            node.x2 = node.x1 + X_INTERVAL
+            node.y1 = node.y - X_INTERVAL
+            node.y2 = node.y1 + X_INTERVAL
+
 
             # # testing
-            # self.draw_dots(node.x,node.y)
+            # self.draw_dots(node.x + X_INTERVAL - 4, node.y - X_INTERVAL)
+            # self.draw_dots(node.x + X_INTERVAL + 4, node.y )
+            # self.draw_rec(node.x + X_INTERVAL - 4,node.y - X_INTERVAL,X_INTERVAL,X_INTERVAL)
+
+    def draw_leaf_node(self,node):
+        self.fill_style = BLACK
+        self.begin_path()
+        self.move_to(node.x, node.y - 5)
+        self.line_to(node.x + X_INTERVAL, node.y - 5)
+        self.stroke()
+        self.fill_text(node.taxon.label, node.x + X_INTERVAL, node.y)
+
+
+    def filter_node_selected(self,node, x , y):
+        if node.x1 <= x <= node.x2 and node.y1 <= y <= node.y2:
+            return True
+        return False
+
+    def mouse_clicked(self, x, y):
+        # self.draw_dots(x,y)
+        node_selected = self.tree.find_node(lambda node: self.filter_node_selected(node, x,y))
+
+
+        if node_selected.is_leaf():
+            self.select_leaf_node(node_selected)
+            if hasattr(node_selected, 'selected'):
+                node_selected.selected = not node_selected.selected
+            else:
+                node_selected.selected = True
+
 
     def draw_dots(self,x,y):
-        self.fill_style = 'red'
+        self.fill_style = RED
         self.fill_circle(x, y, 1)
-        self.fill_style = 'black'
+        self.fill_style = BLACK
 
     def draw_line(self,x1,x2,y1,y2):
-        self.fill_style = 'red'
+        self.fill_style = RED
         self.begin_path()
         self.move_to(x1, y1)
         self.line_to(x2,y2)
         self.stroke()
 
-        self.fill_style = 'black'
+        self.fill_style = BLACK
 
-    def draw_rec(self,x,y,width,height):
-        self.fill_style = BLUE
-        self.stroke_style = BLACK
-
+    def draw_rec(self,x,y,width,height,color):
+        self.fill_style = color
         # fill_rect(x, y, width, height=None):
         # stroke_rect(x, y, width, height=None):
         self.fill_rect(x, y, width, height)
-        self.stroke_rect(x, y, width, height)
+
+    def select_leaf_node(self,node_selected):
+        if not hasattr(node_selected, 'selected') or node_selected.selected == False:
+            color_index = self.subtree_color_used.index(1)
+            self.subtree_color_used[color_index] = 0
+            color = self.subtree_color_list[color_index]
+            node_selected.color = color
+            self.draw_rec(node_selected.x1, node_selected.y1, node_selected.x2 - node_selected.x1,
+                          node_selected.y2 - node_selected.y1, color)
+            self.draw_leaf_node(node_selected)
+        else:
+            self.draw_rec(node_selected.x1, node_selected.y1, (node_selected.x2 - node_selected.x1) + 2,
+                          node_selected.y2 - node_selected.y1, BLANK)
+            self.draw_leaf_node(node_selected)
+            color_index = self.subtree_color_list.index(node_selected.color)
+            self.subtree_color_used[color_index] = 1
+
 
 
 class tcCanvas(MyCanvas): # Canvas for Tree Collection
--- myTree.py
+++ myTree.py
@@ -29,6 +29,10 @@ class myTree:
         mrca = self.rt.mrca(taxon_labels=outgroup_taxon)
         self.rt.reroot_at_edge(mrca.edge, update_bipartitions=False)
 
+    def add_tree_collection(self,treefile=None,type="newick"):
+        self.tc = dendropy.TreeList.get_from_path(treefile, schema=type)
+
+
 def print_tree(tree):
     print(tree.as_ascii_plot())
 
@@ -38,5 +42,32 @@ def get_node_list(tree):
 def get_leaf_node_amount(tree):
     return sum(1 for node in tree.leaf_node_iter())
 
-# mytree = myTree(treefile = "../../Data/69species/astral.FAA.trim50genes.final.tre")
-# print(get_leaf_node_amount(mytree.rt))
+'''
+mytree = myTree(treefile = "Data/69species/astral.FAA.trim50genes.final.tre")
+mytree.add_tree_collection(treefile = "Data/69species/tc.tre")
+for tree in mytree.tc:
+    print(tree)
+'''
+
+"""
+def filter_node_selected(node, x, y):
+    if hasattr(node, 'x1'):
+        if node.x1 <= x <= node.x2 and node.y1 <= y <= node.y2:
+            print(node.taxon)
+            return True
+        return False
+
+
+def add_coordinate(node):
+    if node.is_leaf() and node.taxon.label == "Uronema sp":
+        node.x1 = 10
+        node.x2 = 20
+        node.y1 = 10
+        node.y2 = 20
+
+mytree = myTree(treefile = "../../Data/69species/astral.FAA.trim50genes.final.tre")
+print(get_leaf_node_amount(mytree.rt))
+mytree.rt.find_node(lambda node: add_coordinate(node))
+node_selected = mytree.rt.find_node(lambda node: filter_node_selected(node, 13, 13))
+print(node_selected)
+"""
